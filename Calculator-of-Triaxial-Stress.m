2238e82008c0f82aa3548859de8fb62c11ff1cc3
greedbob <greedbob@foxmail.com>
2016-12-11 23:38:46 +0800

    Symbolic Computation

--- Calculator-of-Triaxial-Stress.m
+++ Calculator-of-Triaxial-Stress.m
@@ -1,11 +1,27 @@
-﻿function [pn1,pn2]=bo2()
+﻿function fun(   )
 
-a=input('输入角度');
-b=input('输入三向应力状态');
-p=a*b;
-pn=p*p';
-ans1=a(1)*b(1)^2 + a(5)*b(2)^2 + a(9)*b(3)^2 + 2a(2)*b(1)*b(2) + 2a(3)*b(2)*b(3) + 2a(6)*b(1)*b(3);
-ans2=(pn^2-ans1^2)^0.5;
-%% left=[cos(a),sin(a);-sin(a),cos(a)];
-% x=left*b*(left');
-% disp(x);
+syms n1 n2 n3 x y z t1 t2 t3 real;
+
+ n=[n1,n2,n3]';
+ A=[x,t1,t2;t1,y,t3;t2,t3,z];
+ 
+%给定方向应力状态符号运算
+ 
+ n=n/(n(1)^2+n(2)^2+n(3)^2)^0.5;
+ p=A*n;
+ xx=p'*n;
+ tt=(p'*p-xx^2)^0.5;
+
+ AA=input('三向应力状态');
+
+%主应力计算
+
+syms b real;
+ B=A-[b,0,0;0,b,0;0,0,b];
+ bb=det(B)==0;
+ bbb=subs(bb,{x,y,z,t1,t2,t3},{AA(1),AA(5),AA(9),AA(2),AA(3),AA(6)});
+ ss=solve(bbb);
+ ss=eval(ss)
+ 
+
+ disp('主应力');
